09be13a0704920e3d45ed8988db989dee255b4a9
Ambert Jc <jc.ambert@free.fr>
2026-07-14 12:09:47 +0200

    feat(v3): register authenticated client services

--- src/CapMethod.Saas/CapMethod.Saas.Client/Program.cs
+++ src/CapMethod.Saas/CapMethod.Saas.Client/Program.cs
@@ -1,4 +1,5 @@
 using CapMethod.Saas.Client;
+using CapMethod.Saas.Client.Auth;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,5 +7,12 @@ WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(_ => new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+});
+builder.Services.AddScoped<BrowserTokenStore>();
+builder.Services.AddScoped<CapMethodApiClient>();
 
 await builder.Build().RunAsync();
